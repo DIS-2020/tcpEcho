98f15748c4ad8a3dd42185cc5f3e85d7de692c3d
mathiasrotheisen <matrote@hotmail.com>
2020-09-08 00:36:16 +0200

    made an assignment

--- echoClient.js
+++ echoClient.js
@@ -1,16 +1,15 @@
-var net = require('net');
+//Kald igen net bilblioteket og gem i en variabel
 
 var client = new net.Socket();
-client.connect(4001, 'host private IP', function() {
-	console.log('Connected');
-	client.write("Hi server");
+
+client.connect(/* Ønskede Port */, /* Destinations IP-adressen */, function() {
+	console.log('Connected'); // Skriver til vores log når vi er tilsluttet
+	// Skriv noget til serveren med .write
 });
 
-client.on('data', function(data) {
-	console.log('Received: ' + data);
-	client.destroy(); // kill client after server's response
-});
+// Brug her client.on til at behandle data i modtager fra serveren. 
 
+//Nedenstående lukker jeres forbindelse ved termination
 client.on('close', function() {
 	console.log('Connection closed');
 });
--- echoServer.js
+++ echoServer.js
@@ -1,18 +1,17 @@
-var net = require("net");
 
-net.createServer(function(socket) {
-console.log("New connection.");
-socket.setEncoding("utf8");
-socket.write("Hello! You can start typing. Type 'quit' to end connection.\n");
+//Hent 'net' biblioteket og gem det i en variabel
+var net = "???"
 
-socket.on("data", function(data) {
-    console.log("got: ", data.toString());
-    if(data.trim().toLowerCase() === "quit") {
-        socket.write("Goodbye!");
-        return socket.end();
-    }
-});
+//Herunder skal net's createServer-funktion benyttes med en callback-funktion der tager et socket objekt
+/* Kald create server */(function(socket) {
+socket.setEncoding("utf8"); //Den her skal I ikke røre
+
+//Skriv noget til potentielle lyttere her
+
+//Her skal I skrive hvad jeres socket skal gøre, når den modtager data med en callback-funktion
+
+// Vi lukket socket ned ved afslutning
 socket.on("end", function() {
     console.log("Client connection is terminated.")
 });
-}).listen(4001, 'host private IP');
+}).listen(/* indsæt jeres port her*/, /* Indsæt jeres private IP-adresse her*/);
