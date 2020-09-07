3595114ddeed36d915029268b33e46ba76326929
mathiasrotheisen <matrote@hotmail.com>
2020-09-08 00:50:41 +0200

    small fixes

--- echoClient.js
+++ echoClient.js
@@ -1,14 +1,13 @@
 var net = require('net');
 
 var client = new net.Socket();
-client.connect(4001, 'host private IP', function() {
+client.connect(4000, 'host private IP', function() {
 	console.log('Connected');
 	client.write("Hi server");
 });
 
 client.on('data', function(data) {
 	console.log('Received: ' + data);
-	client.destroy(); // kill client after server's response
 });
 
 client.on('close', function() {
--- echoServer.js
+++ echoServer.js
@@ -3,16 +3,13 @@ var net = require("net");
 net.createServer(function(socket) {
 console.log("New connection.");
 socket.setEncoding("utf8");
-socket.write("Hello! You can start typing. Type 'quit' to end connection.\n");
+socket.write("Hello Client!");
 
 socket.on("data", function(data) {
+    socket.write(data.toString())
     console.log("got: ", data.toString());
-    if(data.trim().toLowerCase() === "quit") {
-        socket.write("Goodbye!");
-        return socket.end();
-    }
 });
 socket.on("end", function() {
     console.log("Client connection is terminated.")
 });
-}).listen(4001, 'host private IP');
+}).listen(4000, "host private IP");
